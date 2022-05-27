de83d4b6dfa8b40234b68afe7b9067adbee4c266
Reddybathuni <yourgitemail@example.com>
2022-05-27 16:54:43 +0530

    resource added

---
+++ 20220527154647_resource.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE resource (
+    id VARCHAR(64) NOT NULL,
+    name VARCHAR(64),
+    description text,
+    created_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+    modified_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+    PRIMARY KEY(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS resource;
+-- +goose StatementEnd
